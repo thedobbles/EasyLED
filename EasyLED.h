2843c8d8dad3be324dd3b6d344320f492805ef18
theDobbles <thedobbles37@gmail.com>
2014-11-19 18:30:50 -0800

    Added Header File
    Name: "EasyLED.h"

---
+++ EasyLED.h
@@ -0,0 +1,22 @@
+/*
+  EasyLED.h - Library for simpler LEDs.
+  Created by M. Simpson, November 19, 2014.
+  Released into the public domain.
+*/
+#ifndef EasyLED_h
+#define EasyLED_h
+
+#include "Arduino.h"
+
+class EasyLED
+{
+  public:
+    EasyLED(int pin);
+    void EasyOn();
+    void EasyOff();
+	void Blink(blinkDelay);
+  private:
+    int _pin;
+};
+
+#endif
